db1f4121c3fea77f92f5079b7df8006ad423a318
Deeokafor <okafor.udahdivine@gmail.com>
2020-08-10 13:46:50 +0100

    added read-file feature to notes-app | modified server.js

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,21 @@
-function notes(){
-	console.log('notes started!');
-}
+//var url = require('url');
+var fs = require('fs');
+var readline = require('readline');
 
 
-module.exports.notes = notes;
+fs.readFile('notes.txt', 'utf8', function(err, data) {
+	if (err) throw err;
+	console.log(data);
+});
+
+//function handleGet() {}
+
+//function handlePost() {}
+
+
+
+//module.exports.handleGet = handleGet;
+//module.exports.handlePost = handlePost;
 
 
 
--- notes-app/server.js
+++ notes-app/server.js
@@ -2,9 +2,19 @@ console.log('Node Server is Live!');
 
 var http = require('http');
 var app = require('./app');
+var port = 3000;
+var hostname = 'localhost';
 
-http.createServer( function (req, res) {
-	res.write('notes app server is live!');
-	res.end();
-}).listen(3000, 'localhost');
+app = http.createServer( function (req, res) {
+/*	
+	if (req.method === 'GET') {
+		return app.handleGet(req, res);
+	} else if (req.method === 'POST') {
+		return app.handlePost(req, res);
+	}
+*/
+
+	res.write(`Server is live on port ${port} \n`);
+	res.end(`You can interact with console now`);
+}).listen(port, hostname);
 
